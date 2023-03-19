2ae008e7ad6931ea5e9123d094372dc2c13b5926
akashmondal2000 <mondalakash494@gmail.com>
2023-03-19 21:37:04 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import Clock from "./Components/Clock";
+import './App.css';
 
 
 
@@ -10,10 +11,48 @@ const App=()=> {
   const [timerHours,setTimerHours] = useState();
   const [timerSeconds,setTimerSeconds] = useState();
 
-  
+  let interval;
+
+  const startTimer= ()=>{
+    const countDownDate = new Date("March 22,2023").getTime();
+
+    interval = setInterval(()=>{
+      const now = new Date().getTime();
+
+      const distance = countDownDate-now;
+
+      const days = Math.floor(distance / (24*60*60*1000));
+      const hours = Math.floor(distance % (24*60*60*1000) / (1000*60*60));
+      const munits = Math.floor(distance % (60*60*1000) / (1000*60));
+      const Seconds = Math.floor(distance % (60*1000) / (1000));
+
+      if (distance < 0){
+        //stop Timer
+        clearInterval(interval.current);
+      }else{
+        // constantly update the timer
+
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMunits(munits);
+        setTimerSeconds(Seconds);
+      }
+
+
+    });
+  };
+
+  useEffect (()=>{
+    startTimer();
+  })
+
   return (
     <div className="App">
-    <Clock/>
+    <Clock timerDays = {timerDays}
+          timerHours = {timerHours}
+          timerMunits = {timerMunits}
+          timerSeconds = {timerSeconds}
+    />
     </div>
   );
 }
--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -1,11 +1,47 @@
 import React from 'react'
 
-const Clock = () => {
+const Clock = ({timerDays,timerHours,timerMunits,timerSeconds}) => {
   return (
-    <div>
-        <h1>Akash</h1>
-    </div>
+    <>
+        <section className="timer-container">
+            <section className="timer">
+
+                <div className="clock">
+                    <section>
+                       <p>{timerDays}</p>
+                       <small>Days</small> 
+                    </section>
+                    <span>:</span>
+
+                    <section>
+                       <p>{timerHours}</p>
+                       <small>Hours</small> 
+                    </section>
+                    <span>:</span>
+
+                    <section>
+                       <p>{timerMunits}</p>
+                       <small>Munits</small> 
+                    </section>
+                    <span>:</span>
+
+                    <section>
+                       <p>{timerSeconds}</p>
+                       <small>Seconds</small> 
+                    </section>
+                </div>
+            </section>
+        </section>
+    </>
   )
+}
+
+Clock.defaultProps={
+    timerDays:10,
+    timerHours:10,
+    timerMunits:10,
+    timerSeconds:10,
+
 }
 
 export default Clock;
